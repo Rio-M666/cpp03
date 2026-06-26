ab64321aafca31c193d9ea6eb1d0bd04d198b923
Miyachi Rio <mrio@c6r2s1.42tokyo.jp>
2026-06-26 21:31:37 +0900

    save

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -1,1 +1,19 @@
 
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include <iostream>
+# include <string>
+class ScavTrap
+{
+  private:
+  public:
+	ScavTrap();
+	ScavTrap(const std::string name);
+	ScavTrap(const ScavTrap &other);
+	ScavTrap &operator=(const ScavTrap &other);
+	~ScavTrap();
+
+	void guardGate();
+};
+#endif
