febe0672dbf2850085c13666065fffdcf36e3626
RIo Miyachi <131347537+Rio-M666@users.noreply.github.com>
2026-06-27 18:30:04 +0900

    save

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -1,19 +1,23 @@
-
 #ifndef SCAVTRAP_HPP
 # define SCAVTRAP_HPP
 
 # include <iostream>
 # include <string>
-class ScavTrap
+# include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
 {
   private:
+	bool _is_guardGate;
+
   public:
 	ScavTrap();
-	ScavTrap(const std::string name);
+	ScavTrap(const std::string &name);
 	ScavTrap(const ScavTrap &other);
 	ScavTrap &operator=(const ScavTrap &other);
 	~ScavTrap();
 
+	void attack(const std::string &target);
 	void guardGate();
 };
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,1 +1,20 @@
+#include "ScavTrap.hpp"
 
+int main(void)
+{
+	ClapTrap clap("CL4P-TP");
+	ScavTrap scav("SC4V-TP");
+	ScavTrap copy(scav);
+
+	clap.attack("target dummy");
+	clap.takeDamage(3);
+	clap.beRepaired(2);
+
+	scav.attack("bandit");
+	scav.takeDamage(30);
+	scav.beRepaired(10);
+	scav.guardGate();
+
+	copy.attack("copy target");
+	return (0);
+}
